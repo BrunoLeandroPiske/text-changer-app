735c987af02ce0c43ddab1615978762b611fb8c7
Bruno <leandropiskebruno@gmail.com>
2020-09-15 11:04:26 -0300

    implement general functions

--- src/app/main-screen/main-screen.component.ts
+++ src/app/main-screen/main-screen.component.ts
@@ -7,9 +7,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainScreenComponent implements OnInit {
 
+  text1: string = '';
+  text2: string = '';
+  type_convertion: string = '0';
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  changeText1() {
+    this.text2 = this.get_type_and_convert();
+  }
+
+  get_type_and_convert() {
+    switch (this.type_convertion) {
+      case '0': {
+        return this.text1.split("").reverse().join("");;
+        break;
+      }
+      case '1': {
+        return this.text1.toUpperCase();
+        break;
+      }
+      case '2': {
+        return this.text1.toLowerCase();
+        break;
+      }
+      default: {
+        return this.text1;
+      }
+    }
+      
   }
 
 }
