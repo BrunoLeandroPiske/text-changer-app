aafb3a651094d05ddb3ffb9a8878928dcc696029
Bruno <leandropiskebruno@gmail.com>
2020-09-14 21:09:20 -0300

    add main scrren component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainScreenComponent } from './main-screen/main-screen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainScreenComponent
   ],
   imports: [
     BrowserModule,
