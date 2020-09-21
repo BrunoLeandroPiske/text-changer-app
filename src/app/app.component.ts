54a7c6863fb08003f9f7392c8cd5ecfadb8f6701
Bruno <leandropiskebruno@gmail.com>
2020-09-21 10:05:01 -0300

    add base code to show wait screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,22 @@ import { CookieService } from 'ngx-cookie-service';
 })
 export class AppComponent {
   title = 'text-changer-app';
+  accordion_expanded = true;
+
+  convert_text() {
+    console.log('convert_text');
+  }
+
+  convert_number() {
+    console.log('convert_number');
+  }
+
+  convert_units() {
+    console.log('convert_units');
+  }
+
+  convert_money() {
+    console.log('convert_money');
+  }
+
 }
